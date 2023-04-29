14c5353a91699f0b03764aa6d125cabf9a2c9436
Thanakon Kimsan <72403268+konfucius1@users.noreply.github.com>
2023-04-30 02:37:35 +1000

    First commit

---
+++ src/SearchForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchForm = () => {
+  return (
+    <div className="font-sans text-center text-3xl font-bold">
+      Snaplash Images
+    </div>
+  );
+};
+
+export default SearchForm;
